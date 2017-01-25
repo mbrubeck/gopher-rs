471312f63cb6e66903a8cbcfec896a7670a9eb4a
Matt Brubeck <mbrubeck@limpet.net>
2017-01-25 00:05:41 -0800

    Make the protocol generic over Io

--- gopher-core/src/proto.rs
+++ gopher-core/src/proto.rs
@@ -2,13 +2,12 @@ use codec;
 use futures::{stream, Stream};
 use std::io;
 use tokio_core::io::{Io, Framed};
-use tokio_core::net::TcpStream;
 use tokio_proto::pipeline::ServerProto;
 use types::{GopherRequest, GopherResponse};
 
 pub struct GopherServer;
 
-impl ServerProto<TcpStream> for GopherServer {
+impl<T: Io + 'static> ServerProto<T> for GopherServer {
     /// For this protocol style, `Request` matches the codec `In` type
     type Request = GopherRequest;
 
@@ -16,10 +15,10 @@ impl ServerProto<TcpStream> for GopherServer {
     type Response = GopherResponse;
 
     /// A bit of boilerplate to hook in the codec:
-    type Transport = stream::Take<Framed<TcpStream, codec::Server>>;
+    type Transport = stream::Take<Framed<T, codec::Server>>;
     type BindTransport = Result<Self::Transport, io::Error>;
 
-    fn bind_transport(&self, io: TcpStream) -> Self::BindTransport {
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
         // Use .take() to close the stream after a single response.
         Ok(io.framed(codec::Server).take(1))
     }
